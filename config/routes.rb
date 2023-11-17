3ddeb199e11b6a886c15e67718f812e3ccda0f05
Nata-Kalina <kalinichevanata888@gmail.com>
2023-11-17 18:35:19 -0500

    Lesson 3.2: Rails Active Record Associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+    resources :comments
+    #get "/comments", to: "comments#index", as: "comments"
+    #post "/comments", to: "comments#create"
+    #get "/comments/new", to: "comments#new", as: "new_comment"
+    #get "/comments/:id", to: "comments#show", as: "comment"
+    #get "/comments/:id/edit", to: "comments#edit", as: "edit_comment"
+    #patch "/comments/:id", to: "comments#update"
+    #delete "/comments/:id", to: "comments#destroy"
+  end
+
   resources :pages
   #get "/pages", to: "pages#index"
   #post "/pages", to: "pages#create"
@@ -9,8 +19,15 @@ Rails.application.routes.draw do
   #patch "/pages/:id", to: "pages#update"
   #delete "/pages/:id", to: "pages#destroy"
   
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  #get "/posts/:post_id/comments", to: "comments#index", as: "post_comments"
+  #post "/posts/:post_id/comments", to: "comments#create"
+  #get "/posts/:post_id/comments/new", to: "comments#new", as: "new_post_comment"
+  #get "/posts/:post_id/comments/:id", to: "comments#show", as: "post_comment"
+  #get "/posts/:post_id/comments/:id/edit", to: "comments#edit", as: "edit_post_comment"
+  #patch "/posts/:post_id/comments/:id", to: "comments#update"
+  #delete "/posts/:post_id/comments/:id", to: "comments#destroy"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+
+
+
 end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -4,4 +4,13 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  
+  #  setup do
+  #      @page = pages(:one)
+  #  end
+
+  #  test "should get new" do
+  #      get :new
+  #      assert_response :success
+  #  end
 end
